7b08e9c7ee2b5ec37b0fae9d691b8a59fcef6114
geoffowuor <geoffreygroovy@gmail.com>
2026-08-25 09:07:54 +0300

    created url models

--- internals/models/url.go
+++ internals/models/url.go
@@ -1,6 +1,11 @@
 package models
 
+import (
+	"gorm.io/gorm"
+)
+
 type URL struct {
+	gorm.Model
 	ID          string `json:"id"`
 	OriginalURL string `json:"original_url"`
 	ShortCode   string `json:"short_code"`
