ccb5161ab785f8bebccb23b2d01660a3dc2645b8
geoffowuor <geoffreygroovy@gmail.com>
2026-08-25 09:28:55 +0300

    created hndler to generate shortcode

--- internals/handlers/handlers.go
+++ internals/handlers/handlers.go
@@ -4,7 +4,12 @@ import (
 	"github.com/geoffowuor/url-shortener/internals/models"
 	"github.com/gofiber/fiber/v3"
 	"gorm.io/gorm"
+
+	"math/rand"
+	"time"
 )
+
+const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 type URLHandler struct {
 	DB *gorm.DB
@@ -14,6 +19,18 @@ func NewURLHandler(db *gorm.DB) *URLHandler {
 	return &URLHandler{
 		DB: db,
 	}
+}
+
+func GenerateShortCode(length int) string {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	code := make([]byte, length)
+
+	for i := range code {
+		code[i] = letters[r.Intn(len(letters))]
+	}
+
+	return string(code)
 }
 
 func (h *URLHandler) CreateShortURL(c fiber.Ctx) error {
