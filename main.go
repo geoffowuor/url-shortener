c2f8aa14adaf9128326bc24a8315bd85bffc7286
geoffowuor <geoffreygroovy@gmail.com>
2026-08-25 09:12:53 +0300

    db config

--- main.go
+++ main.go
@@ -3,10 +3,21 @@ package main
 import (
 	"log"
 
+	"github.com/geoffowuor/url-shortener/internals/models"
+
 	"github.com/gofiber/fiber/v3"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 func main() {
+
+	db, err := gorm.Open(sqlite.Open("urls.db"), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db.AutoMigrate(&models.URL{})
 
 	app := fiber.New()
 
